f436a61d52250791aee2cc800150dd8c90d7721c
Fernando Nazario <fernandonazario4@gmail.com>
2022-05-12 23:17:46 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from '../mail-adapter'
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '2c1ee40e940a96',
+    pass: '3329e46f07100d'
+  }
+})
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Fernando Nazario <fernando_nazario15@hotmail.com>',
+      subject,
+      html: body
+    })
+  }
+}
